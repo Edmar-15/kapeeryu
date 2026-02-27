a754e661ccb4637da0860503b60a96824be78833
Edmar-15 <0323-3644@lspu.edu.ph>
2026-02-27 22:07:50 +0800

    Added a stylesheets for login, register, and home templates. Add a rule on register form to accept only a institutional account for registration.

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import User
 
+ALLOWED_DOMAINS = [
+     "lspu.edu.ph"   
+    ]
+
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
@@ -9,3 +13,12 @@ class RegisterForm(forms.ModelForm):
         fields = [
             'email', 'username', 'password'
         ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        domain = email.split('@')[-1].lower()
+
+        if domain not in ALLOWED_DOMAINS:
+            raise forms.ValidationError('Use your institutional email')
+        
+        return email
