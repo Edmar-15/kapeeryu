01e13998be653babea902bb6c83fe333204b3eae
Edmar-15 <0323-3644@lspu.edu.ph>
2026-02-27 22:52:08 +0800

    Adjust rules for registration email. Only accepts institution account format.

--- account/forms.py
+++ account/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 from .models import User
+import re
 
 ALLOWED_DOMAINS = [
      "lspu.edu.ph"   
     ]
+
+ID_PATTERN = r"^\d{4}-\d{4}$"
 
 class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
@@ -16,9 +19,19 @@ class RegisterForm(forms.ModelForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        domain = email.split('@')[-1].lower()
+
+        try:
+            local_part, domain = email.split('@')[-1].lower()
+        except ValueError:
+            raise forms.ValidationError('Invalid email format')
 
         if domain not in ALLOWED_DOMAINS:
             raise forms.ValidationError('Use your institutional email')
         
+        if not re.match(ID_PATTERN, local_part):
+            raise forms.ValidationError('Invalid institution email format')
+        
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Account with this email already exists')
+
         return email
